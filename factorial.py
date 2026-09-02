14b7dda868e4a81c5e0dafbfa8219a8f08b80ded
Pugazhenthi-sys <pugazsudhan@gmail.com>
2026-09-02 18:34:33 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,8 +1,8 @@
-n = int(input("Enter a number: "))
+m = int(input("Enter a number: "))
 
 fact = 1
 
-for i in range(1, n + 1):
+for i in range(1, m + 1):
     fact = fact * i
 
 print("Factorial =", fact)
